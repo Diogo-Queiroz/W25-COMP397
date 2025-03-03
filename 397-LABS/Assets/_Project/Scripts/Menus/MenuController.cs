a2fa9dd50dff4da652bbbbffcb6b78b27da5b541
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2025-03-02 19:53:10 -0500

    SceneController, Menu and Game Over UI, AddListener onClick runtime

---
+++ 397-LABS/Assets/_Project/Scripts/Menus/MenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Platformer397
+{
+    public class MenuController : MonoBehaviour
+    {
+        [SerializeField] private Button playBtn;
+        [SerializeField] private Button loadGameBtn;
+        [SerializeField] private Button optionsBtn;
+        [SerializeField] private Button quitBtn;
+
+        private void Start()
+        {
+            playBtn.onClick.AddListener(() => SceneController.Instance.ChangeScene("Gameplay"));
+        }
+    }
+}
