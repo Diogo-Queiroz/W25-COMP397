7997edfcc97acb5c1ba55584997ffe3aa3ba45c8
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2025-03-04 18:46:16 -0500

    Pause System controlling timeScale and components, Audio System with ScriptableObjects, Enemy AI Sensor and Simplest Finite State Machine

--- 397-LABS/Assets/_Project/Scripts/EnemyNavigation.cs
+++ 397-LABS/Assets/_Project/Scripts/EnemyNavigation.cs
@@ -12,6 +12,10 @@ namespace Platformer397
         [SerializeField] private float distanceThreshold = 1.0f;
         private int index = 0;
         private Vector3 destination;
+        // Enemy Sensing Stats
+        [SerializeField] private LayerMask mask; // The layer that correspond to player
+        [SerializeField] private int viewDistance = 10;
+        [SerializeField] private EnemyStates state = EnemyStates.Patrolling;
 
         private void Awake()
         {
@@ -35,11 +39,43 @@ namespace Platformer397
 
         void Update()
         {
-            if (Vector3.Distance(destination, transform.position) < distanceThreshold)
+            switch(state)
             {
-                index = (index + 1) % waypoints.Count;
-                destination = waypoints[index].position;
-                agent.destination = destination;
+                case EnemyStates.Patrolling:
+                    if (Vector3.Distance(destination, transform.position) < distanceThreshold)
+                    {
+                        index = (index + 1) % waypoints.Count;
+                        destination = waypoints[index].position;
+                    }
+                    break;
+                case EnemyStates.Chasing:
+                    // Start chasing the player while visible
+                    destination = player.gameObject.transform.position;
+                    break;
+                default:
+                    Debug.LogError("State not configured", this);
+                    break;
+            }
+            agent.destination = destination;
+        }
+
+        private void FixedUpdate()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, viewDistance, mask))
+            {
+                if (hit.transform.gameObject.CompareTag("Player"))
+                {
+                    state = EnemyStates.Chasing;
+                }
+                Debug.Log("Hit " + hit.transform.gameObject.name);
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            }
+            else
+            {
+                state = EnemyStates.Patrolling;
+                Debug.Log("Hit nothing");
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * viewDistance, Color.yellow);
             }
         }
 
