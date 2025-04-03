5f9b6cfbb28c5cd645642b1f027d6f80cef61335
Ido Isaac <Idoisaac93@gmail.com>
2025-04-02 20:18:39 -0400

    Lab 8

---
+++ 397-LABS/Assets/_Project/Scripts/FactoryPattern/RoadAgent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using KBCore.Refs;
+
+namespace Platformer397
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class RoadAgent : MonoBehaviour, IAgent
+    {
+        [SerializeField, Self] private NavMeshAgent agent;
+        private Vector3 target;
+        private void OnValidate()
+        {
+            this.ValidateRefs();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Vector3.Distance(transform.position, target) < 1.5f)
+            {
+                CompleteJob();
+            }
+        }
+
+        public void Navigate(Vector3 destination)
+        {
+            target = destination;
+            agent.destination = target;
+        }
+
+        public void CompleteJob()
+        {
+            Debug.Log("Job completed");
+            Destroy(gameObject, 1.5f);
+        }
+    }
+}
---
+++ 397-LABS/Assets/_Project/Scripts/FactoryPattern/RoadAgentFactory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Platformer397
+{
+    public class RoadAgentFactory : AbstractFactory
+    {
+            private IEnumerator spawn;
+
+        private void Awake()
+        {
+            spawn = GeneratingAgents();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(spawn);
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                StopCoroutine(spawn);
+            }
+        }
+
+        public IEnumerator GeneratingAgents()
+        {
+            var waitTime = new WaitForSeconds(2);
+            while (true)
+            {
+                GenerateAgent();
+                yield return waitTime;
+            }
+        }
+
+        public override void GenerateAgent()
+        {
+            GameObject agent = Instantiate(agentPrefabs[0], spawnLocation.position, spawnLocation.rotation);
+            agent.GetComponent<RoadAgent>().Navigate(spawnTarget.position);
+        }
+    }
+}
