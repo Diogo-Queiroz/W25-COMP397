1c8f037c2f466d140e424be3f5a6ad12c3772a5e
Mizuki <zibing.xin@outlook.com>
2025-04-04 19:18:50 -0400

    Factory Pattern, Coroutine, KyleBanks Refs

---
+++ 397-LABS/Assets/_Project/Scripts/FactoryPatern/RoadAgentFactory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Platformer397
+{
+    public class RoadAgentFactory : AbstractFactory
+    {
+        private IEnumerator routine;
+
+        private void Awake()
+        {
+            routine = GeneratingAgents();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(routine);
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                StopCoroutine(routine);
+            }
+        }
+
+        private IEnumerator GeneratingAgents()
+        {
+            var waitTime = new WaitForSeconds(1f);
+            while (true)
+            {
+                GenerateAgent();
+                yield return waitTime;
+            }
+        }
+
+        public override void GenerateAgent()
+        {
+            GameObject agent = Instantiate(agentPrefabs[0], spawnLocation.position, spawnLocation.rotation);
+            agent.GetComponent<RoadAgent>().Navigate(spawnTarget.position);
+        }
+    }
+}
